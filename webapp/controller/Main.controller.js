6f5126e44ba54454a5b558d914b643c2cc653f9b
matias favale <matias.favale@hotmail.com.ar>
2024-11-07 20:12:40 +0000

    nuevo commit4

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,7 +6,7 @@ function (Controller) {
 
     return Controller.extend("project1.controller.Main", {
         onInit: function () {
-            console.log("V3");
+            console.log("V4");
         }
     });
 });
